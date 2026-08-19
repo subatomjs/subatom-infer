08a8bf13674d384f3d3b57935dcaee654fd3ee11
Kunal-Ch-Das-Official <kunalchandradasofficial@gmail.com>
2026-08-19 23:08:35 +0530

    create documentation

--- src/core/types.ts
+++
@@ -1,5 +0,0 @@
-import type { Schema } from "./schema.js";
-
-export type Infer<S extends Schema<unknown, unknown>> = S["_output"];
-export type Output<S extends Schema<unknown, unknown>> = S["_output"];
-export type Input<S extends Schema<unknown, unknown>> = S["_input"];
